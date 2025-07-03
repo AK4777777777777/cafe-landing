a88a2f5824b6e271437b4bc4868b4dfff4f7fec5
AK4777777777777 <at7503852@gmail.com>
2025-07-03 11:49:34 +0530

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,22 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.hamburger-menu');
+const closeIcon = document.querySelector('.close-icon-HB');
+const mobileLinks = document.querySelectorAll('.mobile-only-a');
 
 hamburger.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
+  mobileMenu.classList.add('active');
+  document.body.classList.add('no-scroll');
 });
 
+closeIcon.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  document.body.classList.remove('no-scroll');
+});
 
+mobileLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    console.log('Link clicked'); // 💡 Debug check
+    mobileMenu.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
+});
